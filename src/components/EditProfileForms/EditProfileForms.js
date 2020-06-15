1243517df72b54342e97209bf9cd6a3fb3c26e8b
lee-wood <leemaewood@gmail.com>
2020-06-15 19:06:20 -0400

    applicant edit form moved to main ApplicantProfile component

--- src/components/EditProfileForms/EditProfileForms.js
+++ src/components/EditProfileForms/EditProfileForms.js
@@ -33,6 +33,17 @@ export const EditProfile = (props) => {
   const classes = useStyles();
   const userType = props.userType;
 
+  // const handleChange = (event) => {
+  //   props.setProfile({
+  //     ...props.profile,
+  //     [event.target.name]: event.target.value
+  //   });
+  // };
+
+  // const handleSumbit = event => {
+
+  // }
+
   if (userType === "writer") {
     return (
       <>
@@ -154,7 +165,120 @@ export const EditProfile = (props) => {
       
   } else {
     return (
-      <h4>Applicant Edit Form Here.</h4>
+      <>
+        <div className={classes.editDiv}>
+          <div className="name-edit">
+            <h3 className={classes.editTitle}>
+              Name:
+            </h3>
+            <Input
+              className="first-name-input"
+              name="first_name"
+              type="text"
+              autoFocus={true}
+              value={props.profile.first_name}
+              onChange={props.editHandleChange}
+            />
+            <Input
+              className="last-name-input"
+              name="last_name"
+              type="text"
+              value={props.profile.last_name}
+              onChange={props.editHandleChange}
+            />
+          </div>
+          <div className={classes.location}>
+            <h3 className={classes.editTitle}>
+              Location:
+            </h3>
+            <label>
+              City: {" "}
+              <Input
+                className="edit-profile-input"
+                name="city"
+                type="text"
+                value={props.profile.city}
+                onChange={props.editHandleChange}
+              />
+            </label>
+            <label>
+            State: {" "}   
+            <Input
+              className="edit-profile-input"
+              name="state"
+              type="text"
+              value={props.profile.state}
+              onChange={props.editHandleChange}
+            />
+          </label>
+          <label>
+            Zip: {" "}
+            <Input
+              className="edit-profile-input"
+              name="zip"
+              type="text"
+              value={props.profile.zip}
+              onChange={props.editHandleChange}
+            />
+          </label>
+          <label>
+            Country: {" "}
+            <Input
+              className="edit-profile-input"
+              name="country"
+              type="text"
+              value={props.profile.country}
+              onChange={props.editHandleChange}
+            />
+          </label>
+        </div>
+
+        <div className={classes.moreInfo}>
+          <h3 className={classes.editTitle}>
+            More About Me:
+          </h3>
+          <label>
+            Bio: {" "}  
+            <Input
+              className="edit-profile-input"
+              name="bio" 
+              type="text"
+              multiline={true}
+              fullWidth={true}
+              value={props.profile.bio}
+              onChange={props.editHandleChange}
+            />
+          </label>
+          <label>
+            Sector: {" "}
+            <Input 
+              className="edit-profile-input"
+              name="sector"
+              type="text"
+              value={props.profile.sector}
+              onChange={props.editHandleChange}
+            />
+          </label>
+          <label>
+            Website: {" "}
+            <Input 
+              className="edit-profile-input"
+              name="website"
+              type="text"
+              value={props.profile.website}
+              onChange={props.editHandleChange}
+            />  
+          </label>      
+        </div>
+
+        <Button
+          type="submit"
+          onClick={props.handleSubmit}
+        >
+          Save
+        </Button>
+        </div>
+      </>
     )
   }
   //if userType = writer return thing else return other thing...
